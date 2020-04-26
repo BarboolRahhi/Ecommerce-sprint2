299ded48847fe6f55853fc640ed313c395f4fb5a
Rahhi Barbool <42481759+BarboolRahhi@users.noreply.github.com>
2020-04-26 16:48:22 +0530

    home item and product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,28 @@ import { HomeComponent } from "./home/home.component";
 import { UserComponent } from "./user/user.component";
 import { SignInComponent } from "./user/sign-in/sign-in.component";
 import { SignUpComponent } from "./user/sign-up/sign-up.component";
+import { ProductComponent } from "./product/product.component";
+import { ProductListComponent } from "./product/product-list/product-list.component";
+import { ProductDetailsComponent } from "./product/product-details/product-details.component";
+import { HomeItemComponent } from "./home-item/home-item.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: "home", pathMatch: "full" },
-  { path: "home", component: HomeComponent },
+  {
+    path: "",
+    component: HomeComponent,
+    children: [
+      { path: "", redirectTo: "/home", pathMatch: "full" },
+      { path: "home", component: HomeItemComponent },
+      {
+        path: "product",
+        component: ProductComponent,
+        children: [
+          { path: "list", component: ProductListComponent },
+          { path: "details/:pid", component: ProductDetailsComponent },
+        ],
+      },
+    ],
+  },
   {
     path: "user",
     component: UserComponent,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,33 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { CarouselComponent } from "./carousel/carousel.component";
-import { UserComponent } from './user/user.component';
-import { SignInComponent } from './user/sign-in/sign-in.component';
-import { SignUpComponent } from './user/sign-up/sign-up.component';
-import { HomeComponent } from './home/home.component';
-import { ProductComponent } from './product/product.component';
-import { ProductListComponent } from './product/product-list/product-list.component';
-import { ProductDetailsComponent } from './product/product-details/product-details.component';
-import { CartComponent } from './cart/cart.component';
-import { OrderComponent } from './order/order.component';
+import { UserComponent } from "./user/user.component";
+import { SignInComponent } from "./user/sign-in/sign-in.component";
+import { SignUpComponent } from "./user/sign-up/sign-up.component";
+import { HomeComponent } from "./home/home.component";
+import { ProductComponent } from "./product/product.component";
+import { ProductListComponent } from "./product/product-list/product-list.component";
+import { ProductDetailsComponent } from "./product/product-details/product-details.component";
+import { CartComponent } from "./cart/cart.component";
+import { OrderComponent } from "./order/order.component";
+import { HomeItemComponent } from './home-item/home-item.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, CarouselComponent, UserComponent, SignInComponent, SignUpComponent, HomeComponent, ProductComponent, ProductListComponent, ProductDetailsComponent, CartComponent, OrderComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CarouselComponent,
+    UserComponent,
+    SignInComponent,
+    SignUpComponent,
+    HomeComponent,
+    ProductComponent,
+    ProductListComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    OrderComponent,
+    HomeItemComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
